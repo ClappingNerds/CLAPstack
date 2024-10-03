36bba303da38771d517cbc204a09ccc0485d05e1
Abhishek Mallepalli <mallepalliabhishek@gmail.com>
2024-10-03 03:23:25 -0700

    added template env file. Added optional nvidia configs for pytorch container. Replaced hard coded absolute project directory with environment variable. Minor cleanups. Added toggleable entrypoint lines to disable containers.

--- scripts/init_airflow.sql
+++ scripts/init_airflow.sql
@@ -1,10 +1,2 @@
--- Create the Airflow database
-;
-
--- Create the Airflow user with a password
-CREATE USER airflow WITH PASSWORD 'airflow_pass';
-
+CREATE USER airflow WITH SUPERUSER PASSWORD 'airflow_pass';
 CREATE DATABASE airflow_db WITH OWNER airflow;
-
--- Grant privileges on the database to the user
-GRANT ALL PRIVILEGES ON DATABASE airflow_db TO airflow;
